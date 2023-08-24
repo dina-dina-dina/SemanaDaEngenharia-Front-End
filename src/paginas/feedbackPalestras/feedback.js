975eb561e0cb5bb8d97f0a4e10c687b1f27d0b43
Cortada-PC <caio.cortada@unesp.br>
2023-08-23 23:46:00 -0300

    Feat: Abertura ler QR Code antes

--- src/paginas/feedbackPalestras/feedback.js
+++ src/paginas/feedbackPalestras/feedback.js
@@ -1,4 +1,4 @@
-import { useNavigate,useLocation  } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import '../../paginas/novoEvento/novoEvento.css'
 import React, { useState, useContext, useEffect } from 'react';
 import axios from '../../axios';
@@ -8,7 +8,7 @@ import Palestras from '../palestras/palestras';
 
 function FeedbackPalestra() {
     const [nome, setNome] = useState(String)
-    const [Feedback, setFeedback] = useState(Date)
+    const [Feedback, setFeedback] = useState(String)
     const [cargaHoraria, setCargaHoraria] = useState(Date)
     const navigate = useNavigate();
     const [Dia, setDia] = useState(Date)
@@ -35,35 +35,43 @@ function FeedbackPalestra() {
     const idUsuario = localStorage.getItem('user')
 
 
-    
+
     function handleClick() {
-        navigate('/qrCode2',{state:{nome:location.state.nome,idUsuario:idUsuario,feedback:Feedback,idPalestraEvento:idUrl}});
+        if (idUrl == 20) {
+            setFeedback("Abertura")
+        }
+        navigate('/qrCode2', { state: { nome: location.state.nome, idUsuario: idUsuario, feedback: Feedback, idPalestraEvento: idUrl } });
     }
 
-      useEffect(() => {
+    useEffect(() => {
         getEvento();
         if (localStorage.getItem('authenticated') !== 'true') navigate('/');
-        
-       
+
+
     }, [])
-      
+
 
     return (
         <div className='containerNew'>
             <div className='formulario2'>
-                    <span id = 'title'>FEEDBACK DA PALESTRA:</span>
-                    <div className='textpal'>{location.state.nome}</div>
+                <span id='title'>FEEDBACK DA PALESTRA:</span>
+                <div className='textpal'>{location.state.nome}</div>
 
-                    <div className='texto'>Escreva o feedback e gere seu QRCode para verificação</div>
+                <div className='texto'>Escreva o feedback e gere seu QRCode para verificação</div>
                 <div className='itensForms'>
                     <form>
-                        <div className='data'>
-                            <label id='labelStyle'>Feedback</label>
-                            <input id='inputStyle' placeholder='feedback'type={'text'} onChange={(event) => setFeedback(event.target.value)}></input>
-                        </div>
-                        
+                        {
+                            (idUrl == 20)
+                                ? <div style={{ fontFamily: 'Poppins', textAlign: 'center', fontWeight: 'bold' }}>Esse QR Code deve ser lido na entrada da Abertura</div>
+
+                                :<div className='data'>
+                                <label id='labelStyle'>Feedback</label>
+                                <input id='inputStyle' placeholder='feedback' type={'text'} onChange={(event) => setFeedback(event.target.value)}></input>
+                                </div>}
+
+
                         <div className='botao' >
-                            <button  type = 'button' onClick={() => {handleClick()}}>Gerar QRCode</button>
+                            <button type='button' onClick={() => { handleClick() }}>Gerar QRCode</button>
                         </div>
                     </form>
                 </div>
