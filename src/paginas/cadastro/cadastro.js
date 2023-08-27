ce769bc33b67b7db96eccc21730bfca7a39cdbc9
Cortada-PC <caio.cortada@unesp.br>
2023-08-27 17:16:23 -0300

    Feat: ajuste cadastro

--- src/paginas/cadastro/cadastro.js
+++ src/paginas/cadastro/cadastro.js
@@ -41,13 +41,41 @@ function Cadastro() {
             selected={option.value === cursoValue}>{option.text}
         </option>
     })
+    function validateEmail(entrada) {
+        const emailInput = entrada;
+        // Regular expression pattern to match a valid email format
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+        return emailPattern.test(emailInput);
+    }
     async function handleClick() {
         try {
             if (senha != senha2) {
                 throw new Error("Senha incompatível!")
             }
+
+            if(!validateEmail(email)){
+                throw new Error("Email inválido!")
+            }
+
+            if(!nome){
+                throw new Error("Insira um nome!")
+            }
+           
+        
             const response = await axios.post("/registerAluno", {
+            "email": email,
+            "nome": nome,
+            "senha": senha,
+            "ra": ra,
+            "idTurma": turma,
+            "estagio": isChecked,
+            "telefone": telefone,
+            "curso": cursoValue
+
+        }).catch(err => alert(err));
+        console.log(
+            {
                 "email": email,
                 "nome": nome,
                 "senha": senha,
@@ -56,87 +84,75 @@ function Cadastro() {
                 "estagio": isChecked,
                 "telefone": telefone,
                 "curso": cursoValue
-
-            }).catch(err => alert(err));
-            console.log(
-                {
-                    "email": email,
-                    "nome": nome,
-                    "senha": senha,
-                    "ra": ra,
-                    "idTurma": turma,
-                    "estagio": isChecked,
-                    "telefone": telefone,
-                    "curso": cursoValue
-                }
-            )
-            if (response) {
-                alert("Aluno cadastrado com sucesso!")
-                navigate("/");
             }
-        } catch (err) {
-            alert(err)
+        )
+        if (response) {
+            alert("Aluno cadastrado com sucesso!")
+            navigate("/");
         }
+    } catch (err) {
+        alert(err)
     }
+}
 
-    return (
-        <div className="containercadas">
-            <div className="logincadas">
-                <div className='formulariocadas'>
-                    <form>
-                        <span className='tituloide' style={{ textAlign: 'center' }}>Faça seu Cadastro!</span>
-                        <div className='input'>
-                            <div className='turma'>
-                                <label>Nome</label>
-                                <input type='text' alt="Input de nome" placeholder='Nome' onChange={(event) => setNome(event.target.value)}></input>
-                            </div>
-                            <div className='turma'>
-                                <label>RA</label>
-                                <IMaskInput type='number' placeholder='000000000' mask="000000000" onChange={(event) => setRA(event.target.value)} />
-                            </div>
-                            <div className='turma'>
-                                <label>Turma</label>
-                                <IMaskInput type='number' placeholder='00' mask="00" onChange={(event) => setTurma(event.target.value)} />
-                            </div>
-                            <div className='turma'>
-                                <label>Telefone</label>
-                                <IMaskInput type="tel" id="telefone" name="telefone" placeholder="(00) 00000-0000" mask="(00) 00000-0000" pattern="[0-9]{2}-[0-9]{3}-[0-9]{4}" required onChange={(event) => setTelefone(event.target.value)} />
-                            </div>
-                            <div className="curso">
-                                <label>Curso:</label>
-                                <select onChange={(e) => setCursoValue(e.target.value)}>
-                                    {options}
-                                </select>
-                            </div>
-                            <div className='turma'>
-                                <label>Email</label>
-                                <input type='email' alt="Input de email" placeholder='Email' onChange={(event) => setEmail(event.target.value)}></input>
-                            </div>
-                            <div className='turma'>
-                                <label>Senha</label>
-                                <input type='password' placeholder='Senha' onChange={(event) => setSenha(event.target.value)}></input>
-                            </div>
-                            <div className='turma'>
-                                <label>Confirme sua senha</label>
-                                <input type='password' placeholder='Confirme a senha' onChange={(event) => setSenha2(event.target.value)}></input>
-                            </div>
-                            <div className='estagioSuperior'>
-                                <div className='estagio'>
-                                    <label>Está estagiando?</label>
-                                    <input className={isChecked ? "checked" : ""} checked={isChecked} type='checkbox' onChange={clicker}></input>
-                                </div>
-                            </div>
-
-                            <div className='buttoncadas' >
-                                <button type='button' onClick={() => handleClick()}>ENTRAR</button>
-                            </div>
-
+return (
+    <div className="containercadas">
+        <div className="logincadas">
+            <div className='formulariocadas'>
+                <form>
+                    <span className='tituloide' style={{ textAlign: 'center' }}>Faça seu Cadastro!</span>
+                    <div className='input'>
+                        <div className='turma'>
+                            <label>Nome</label>
+                            <input type='text' alt="Input de nome" placeholder='Nome' onChange={(event) => setNome(event.target.value)}></input>
                         </div>
-                    </form>
-                </div>
+                        <div className='turma'>
+                            <label>RA</label>
+                            <IMaskInput type='number' placeholder='000000000' mask="000000000" onChange={(event) => setRA(event.target.value)} />
+                        </div>
+                        <div className='turma'>
+                            <label>Turma</label>
+                            <IMaskInput type='number' placeholder='00' mask="00" onChange={(event) => setTurma(event.target.value)} />
+                        </div>
+                        <div className='turma'>
+                            <label>Telefone</label>
+                            <IMaskInput type="tel" id="telefone" name="telefone" placeholder="(00) 00000-0000" mask="(00) 00000-0000" pattern="[0-9]{2}-[0-9]{3}-[0-9]{4}" required onChange={(event) => setTelefone(event.target.value)} />
+                        </div>
+                        <div className="curso">
+                            <label>Curso:</label>
+                            <select onChange={(e) => setCursoValue(e.target.value)}>
+                                {options}
+                            </select>
+                        </div>
+                        <div className='turma'>
+                            <label>Email</label>
+                            <input type='email' alt="Input de email" placeholder='Email' onChange={(event) => setEmail(event.target.value)}></input>
+                        </div>
+                        <div className='turma'>
+                            <label>Senha</label>
+                            <input type='password' placeholder='Senha' onChange={(event) => setSenha(event.target.value)}></input>
+                        </div>
+                        <div className='turma'>
+                            <label>Confirme sua senha</label>
+                            <input type='password' placeholder='Confirme a senha' onChange={(event) => setSenha2(event.target.value)}></input>
+                        </div>
+                        <div className='estagioSuperior'>
+                            <div className='estagio'>
+                                <label>Está estagiando?</label>
+                                <input className={isChecked ? "checked" : ""} checked={isChecked} type='checkbox' onChange={clicker}></input>
+                            </div>
+                        </div>
+
+                        <div className='buttoncadas' >
+                            <button type='button' onClick={() => handleClick()}>ENTRAR</button>
+                        </div>
+
+                    </div>
+                </form>
             </div>
         </div>
-    );
+    </div>
+);
 }
 
 export default Cadastro;
