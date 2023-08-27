cc15ae54fdd77b6918bc2b0bdbb5217ce20fcb81
GiulianoFaria <giuliano.de.faria@gmail.com>
2023-08-27 17:05:42 -0300

    Delay do get materias

--- src/paginas/meusProfessores/meusProfessores.js
+++ src/paginas/meusProfessores/meusProfessores.js
@@ -14,48 +14,44 @@ function MeusProfessores() {
     function handleClick1() {
         navigate("/");
     }
-    function handleClick2() {
-        navigate("/");
-    }
 
-    const [Materias, Metmaterias] = useState({})
+    // function handleClick2() {
+    //     navigate("/");
+    // }
+
+    const [Materias, setMaterias] = useState({})
     const [minhas, setMinhas] = useState([])
     const [selectedMateriaIds, setSeletas] = useState([])
 
-    const getMinhas = async () => {
+    async function getMinhas() {
         const queryParams = {
             param1: localStorage.getItem('user'),
         }
-        const resposta = await axios.get('/professor/minhas', { params: queryParams })
+        setTimeout(async () => {
+            const resposta = await axios.get('/professor/minhas', { params: queryParams })
+                .catch(err => console.error('Error: ', err));
+            if (resposta) {
+                console.log(resposta.data)
 
-            .catch(err => console.error('Error: ', err));
-        if (resposta) {
-            const array = resposta.data
-
-            const idMateriaValues = array.map(item => item.idMateria);
-
-
-            await setSeletas(idMateriaValues);
-            console.log(idMateriaValues)
-        }
+                var array = resposta.data
+                var idMateriaValues = []
+                for (let i = 0; i < array.length; i++) {
+                    idMateriaValues.push(array[i].idMateria)
+                }
+                setSeletas([...idMateriaValues]);
+                console.log(idMateriaValues)
+            }
+        }, 500)
     }
 
     const getMaterias = async () => {
-        // const queryParams = {
-        //     param1: localStorage.getItem('user'),
-        //     param2: localStorage.getItem('evento')
-        // }
         const response = await axios.get('/professor/materias')
             .catch(err => console.error('Error: ', err));
-        if (response) await Metmaterias(response.data);
-        console.log(response)
-
+        if (response) setMaterias([...response.data]);
+        //console.log(response)
     }
 
-
     async function handleClick3(idMateria) {
-
-
         try {
             const response = axios.post('/professor/cadastroaluno', {
                 "idMateria": idMateria,
@@ -64,32 +60,24 @@ function MeusProfessores() {
             if (response) {
                 getMinhas()
             }
-
-
-
         } catch (err) {
             alert(err)
         }
     }
 
     async function handleClick2(idMateria) {
-
-
         try {
             const response = axios.post('/professor/removerMateria', {
                 "idMateria": idMateria,
                 "idUsuario": localStorage.getItem('user'),
             })
             if (response) {
-                window.location.reload();
+                getMinhas();
             }
-
         } catch (err) {
             alert(err)
         }
     }
-
-
 
     useEffect(() => {
         getMaterias()
@@ -98,44 +86,40 @@ function MeusProfessores() {
 
     return (
         <><div><GoBackArrow /></div>
-        <div className='geral'>
-            
-
-            <div className='foto'>
-                <img src={image} alt="icon" width={"100%"} style={{ alignSelf: 'center' }}></img>
-            </div>
-            {Materias.length > 0 ? (
-                <div className='todas'>
-                    {Materias.map((professor) => (
-                        <div key={professor.idProfessor}>
-                            <div className='profe'>{professor.nome}</div>
-                            <div className='entries'>
-                                {professor.materias.map((materia) => (
-                                    <div key={materia.idMateria}
-                                        onClick={selectedMateriaIds.includes(materia.idMateria)
-                                            ? () => handleClick2(materia.idMateria)
-                                            : () => handleClick3(materia.idMateria)} className='entradaprofe'
-                                        style={
-                                            selectedMateriaIds.includes(materia.idMateria)
-                                                ? { backgroundColor: 'rgb(136, 189, 150)' }
-                                                : { backgroundColor: 'rgb(240, 77, 77)' }
-                                        }>
-                                        <div className='topicoprofe'>{materia.titulo}</div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-
-                    ))}
+            <div className='geral'>
+                <div className='foto'>
+                    <img src={image} alt="icon" width={"100%"} style={{ alignSelf: 'center' }}></img>
                 </div>
-            ) : (
-                <div className='triste'>Nenhum Professor Encontrado 😴</div>
-            )}
-        </div>
-        </>
-        
-    );
+                {Materias.length > 0 ? (
+                    <div className='todas'>
+                        {Materias.map((professor) => (
+                            <div key={professor.idProfessor}>
+                                <div className='profe'>{professor.nome}</div>
+                                <div className='entries'>
+                                    {professor.materias.map((materia) => (
+                                        <div key={materia.idMateria}
+                                            onClick={selectedMateriaIds.includes(materia.idMateria)
+                                                ? () => handleClick2(materia.idMateria)
+                                                : () => handleClick3(materia.idMateria)} className='entradaprofe'
+                                            style={
+                                                selectedMateriaIds.includes(materia.idMateria)
+                                                    ? { backgroundColor: 'rgb(136, 189, 150)' }
+                                                    : { backgroundColor: 'rgb(240, 77, 77)' }
+                                            }>
+                                            <div className='topicoprofe'>{materia.titulo}</div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
 
+                        ))}
+                    </div>
+                ) : (
+                    <div className='triste'>Nenhum Professor Encontrado 😴</div>
+                )}
+            </div>
+        </>
+    );
 }
 
 export default MeusProfessores;
