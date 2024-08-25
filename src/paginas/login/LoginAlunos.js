749175d129dcd0436d6bb8501d90b0a735ff1007
Caio Maciel <89308404+CaioDina@users.noreply.github.com>
2024-08-25 12:57:37 -0300

    feat: spinner

--- src/paginas/login/LoginAlunos.js
+++ src/paginas/login/LoginAlunos.js
@@ -4,10 +4,12 @@ import jpIMG from '../../assets/logosemanaCentrr.png';
 import React, { useState, useContext, useEffect } from 'react';
 import axiosLogin from '../../axiosLogin';
 import image from '../../assets/SemanaEngCirc-removebg.png'
+import { ClipLoader } from "react-spinners";
 
 function UserLogin() {
   const [email, setEmail] = useState(String)
   const [senha, setSenha] = useState(String)
+  let [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   function handleClick3() {
@@ -15,8 +17,11 @@ function UserLogin() {
   }
 
   const RegisterUsuario = async () => {
+ 
     if (email && senha) {
+      setLoading(true)
       try {
+
         console.log('foi ?')
         const response = await axiosLogin.post('/authenticate', {
           "email": email,
@@ -30,52 +35,72 @@ function UserLogin() {
         await localStorage.setItem('user', response.data.user.idUsuarios)
         console.log(response)
         if (response.data.user.isAdmin == true) {
+          setLoading(false)
           navigate("/eventos")
         } else {
+          setLoading(false)
           navigate("/AlunosHomePage")
         }
       }
       catch (err) {
         console.log(err)
+        setLoading(false); // Esconde o spinner independentemente do resultado
         alert('Senha e/ou Usuario incorreto!')
+     
       }
-    } else {
       
+    } else {
+
       alert('Preencha todos os campos')
     }
   }
 
   return (
     <>
-      <div className='tudo'>
-        <div className='fotohome'>
-          <img src={jpIMG} alt="icon" width={"100%"} style={{ alignSelf: 'center' }}></img>
-        </div>
 
-        <div className='formulario'>
-          <div>
-            <div className='tituloide'>Login Semana da Engenharia</div>
-            
-            <div className='input'>
-              <div className='logincamp'>
-                <label>Email</label>
-                <input type='email' alt="Input de nome" placeholder='Email' onChange={(event) => setEmail(event.target.value)}></input>
+      {loading ?
+        <div className='loading'>
+          <ClipLoader
+            color='#097828'
+            loading={loading}
+            size={100}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+        :
+        <div className='tudo'>
+          <div className='fotohome'>
+            <img src={jpIMG} alt="icon" width={"100%"} style={{ alignSelf: 'center' }}></img>
+          </div>
+
+          <div className='formulario'>
+            <div>
+              <div className='tituloide'>Login Semana da Engenharia</div>
+
+              <div className='input'>
+                <div className='logincamp'>
+                  <label>Email</label>
+                  <input type='email' alt="Input de nome" placeholder='Email' onChange={(event) => setEmail(event.target.value)}></input>
+                </div>
+                <div className='logincamp'>
+                  <label>Senha</label>
+                  <input type='password' placeholder='Senha' onChange={(event) => setSenha(event.target.value)}></input>
+                </div>
+                <div className='buttonlogin' >
+                  <button onClick={() => RegisterUsuario()}>ENTRAR</button>
+                </div>
+
               </div>
-              <div className='logincamp'>
-                <label>Senha</label>
-                <input type='password' placeholder='Senha' onChange={(event) => setSenha(event.target.value)}></input>
-              </div>
-              <div className='buttonlogin' >
-                <button onClick={() => RegisterUsuario()}>ENTRAR</button>
-              </div>
-              
+            </div>
+            <div className='buttonlogin2'>
+              <button onClick={() => handleClick3()}>Cadastre-se!</button>
             </div>
           </div>
-          <div className='buttonlogin2'>
-                <button onClick={() => handleClick3()}>Cadastre-se!</button>
-              </div>
         </div>
-      </div>
+      }
+
+
     </>
   );
 }
