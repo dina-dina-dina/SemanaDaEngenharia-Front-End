872a7c31efff784ae15beec0efd348aeb7fb9662
zugs007 <brdos007blue@gmail.com>
2025-08-13 13:25:25 -0300

    Fix - Front Render Axios

--- src/axiosLogin.js
+++ src/axiosLogin.js
@@ -5,7 +5,7 @@ const axios = Axios.create({
     // baseURL: "https://semanaback.onrender.com/",
     // baseURL: "http://dinamicaengjr.kinghost.net:21191/",
     // baseURL: "http://dinamicaengjr.kinghost.net:21191/",
-    baseURL: "http://localhost:21191/",
+    baseURL: "https://semanadaengenharia-back.onrender.com",
     headers: { Auth: 'Simple AUTH'},
     timeout: 50000
 })
